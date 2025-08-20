79dd0303d4641b4bb0d6943a95d9f82c6fe0c353
ahmed-lakosha <ahmed.lakosha94@gmail.com>
2025-08-20 15:12:43 +0300

    Updates

--- theme_indoqat/static/src/js/script.js
+++ theme_indoqat/static/src/js/script.js
@@ -61,8 +61,9 @@ publicWidget.registry.ProjectsCarouselMd = publicWidget.Widget.extend({
             rtl,
             responsive: {
                 0: {items: 1},
-                576: {items: 2},
-                992: {items: 3}
+                576: {items: 1.5},
+                992: {items: 2},
+                1200: {items: 2}
             }
         });
     },
@@ -87,16 +88,3 @@ publicWidget.registry.ProjectsCarouselMd = publicWidget.Widget.extend({
         $el.removeClass('owl-hidden').css('display', '');
     },
 });
-// //Homepage Projects Slider
-// const $owlSlider = $('.projects-carousel');
-// const responsiveParams = {0: {items: 1}, 576: {items: 2}, 768: {items: 2}, 992: {items: 2}, 1200: {items: 3}};
-// $owlSlider?.owlCarousel({
-//     dots: false,
-//     margin: 24,
-//     autoplay: true,
-//     autoplayTimeout: 3000,
-//     rtl: $owlSlider[0]?.classList.contains('ar'),
-//     autoplayHoverPause: true,
-//     rewind: true,
-//     responsive: responsiveParams
-// });
